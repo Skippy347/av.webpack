67160bb2ee083a5186c82fb552ac63c61f3b62aa
Artem <varivonchikartem@gmail.com>
2025-09-13 22:30:06 +0300

    chore(scripts): add eslint:fix script

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
+
 import ReactDOM from "react-dom/client";
 
 import "./index.scss";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
